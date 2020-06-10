23b41d39e8c23d43fdfaa5ffa67c15ff39a10c86
ntpdlp <ntpdlp@gmail.com>
2020-06-10 04:57:58 -0500

    everything in python is object

---
+++ basic_syntax/data-type/python-sequence.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python3
+
+#mutable: able to reassignment
+def python_mutable_array_list():
+    myarr = [1,2,3,4,5]
+    myarr[0] = 100
+    for x in myarr:
+        print (f"{x} ",end=" ")
+
+def python_mutable_dict():
+    mydict = {'one':1, 'two':2, 'three':3}
+    mydict['two'] = 5
+    for k,v in mydict.items():
+        print (f"<{k}:{v}> ", end="\t")
+
+#immutable: not allow to reassignment
+def python_immutable_tuple():
+    mytuple = (1,2,3,4,5)
+    for x in mytuple:
+        print (f"{x} ", end=' ')
+    return mytuple
+
+def python_immutable_range():
+    for x in range(5):
+        print (f"{x} ",end=' ')
+    
+    for y in range (10,15):
+        print (f"{y} ", end=' ')
+
+    for z in range (100,150,10):
+        print (f"{z} ",end='')
+
+
+
+def mutable_immutable():
+    #mutable
+    print ("\nArray|List", end=': ')
+    python_mutable_array_list()
+    print ("\nDictionary", end=': ')
+    python_mutable_dict()
+
+    #immutable
+    print ("\nImmutable Tuple", end=': ')
+    mytuple = python_immutable_tuple()
+    print ("\nImmutable Range", end=': ')
+    python_immutable_range()
+    # #TypeError: 'tuple' object does not support item assignment
+    # print (mytuple[0])
+    # mytuple[0] = 500
+
+def sequence_is_object_type_in_python():
+    x = (1,"apple",5.0,[1,2,3],"orange")
+    y = x # object y will have same address with object x
+    z = ("everything in python","is", "object")
+    w = ("everything in python","is", "object") # object w will have same address with object z
+    print (f"type() function: x is {type(x)}, y is {type(y)}")
+    print (f"id() of these tuples: x is {id(x)} & y is {id(y)}, z is {id(z)} & w is {id(w)}")
+    print (f"id() of items in tuples: x[0] is {id(x[0])}, y is {id(y[0])}")
+    
+
+def main():
+    # mutable_immutable()
+    sequence_is_object_type_in_python()
+
+
+
+
+if __name__ == "__main__":main()
